a30806590134de657d9bed72ba7a7d2e13c2b290
Cassio-Matsushita <superkoalabr@gmail.com>
2022-08-19 22:24:46 -0300

    req57-61

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import renderWithRouter from './tests/helpers/renderWithRouter';
 import App from './App';
 
 test('Farewell, front-end', () => {
-  render(<App />);
+  renderWithRouter(<App />);
   const linkElement = screen.getByText(/TRYBE/i);
   expect(linkElement).toBeInTheDocument();
 });
