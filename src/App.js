36de2b6f948d31688d7d800a94486dbe072135eb
Mariana Werneck <marinhomariana8@gmail.com>
2022-08-23 14:44:08 -0300

    Inicializando testes - incompleto

--- src/App.js
+++ src/App.js
@@ -8,21 +8,24 @@ import './App.css';
 import Foods from './Pages/Foods';
 import Drinks from './Pages/Drinks';
 import Details from './Pages/Details';
+import Provider from './context/Provider';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={ Login } />
-      <Route exact path="/foods" component={ Foods } />
-      <Route exact path="/drinks" component={ Drinks } />
-      <Route exact path="/foods/:idMeal" component={ Details } />
-      <Route exact path="/drinks/:idDrink" component={ Details } />
-      <Route exact path="/foods/:idMeal/in-progress" />
-      <Route exact path="/drinks/:idDrink/in-progress" />
-      <Route path="/profile" component={ Profile } />
-      <Route exact path="/done-recipes" component={ DoneRecipes } />
-      <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
-    </Switch>
+    <Provider>
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/foods" component={ Foods } />
+        <Route exact path="/drinks" component={ Drinks } />
+        <Route exact path="/foods/:idMeal" component={ Details } />
+        <Route exact path="/drinks/:idDrink" component={ Details } />
+        <Route exact path="/foods/:idMeal/in-progress" />
+        <Route exact path="/drinks/:idDrink/in-progress" />
+        <Route path="/profile" component={ Profile } />
+        <Route exact path="/done-recipes" component={ DoneRecipes } />
+        <Route exact path="/favorite-recipes" component={ FavoriteRecipes } />
+      </Switch>
+    </Provider>
   );
 }
 
