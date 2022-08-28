4431f7f1db1e3841b478eafc3c97427fc7b67e08
Cassio-Matsushita <superkoalabr@gmail.com>
2022-08-28 20:26:28 -0300

    req38-43

--- src/tests/RecipeInProgress.test.js
+++ src/tests/RecipeInProgress.test.js
@@ -86,3 +86,55 @@ it('teste os elementos do RecipeInProgress na página /drinks', async () => {
     userEvent.click(finishButton);
     expect(history.location.pathname).toBe('/done-recipes');
 });
+
+it('teste a função de favoritar na página /foods', async () => {   
+  const {history} = renderWithRouter(<Provider> <App /> </Provider>);
+  const emailInput =  screen.getByTestId("email-input");
+  userEvent.type(emailInput, 'lalala@gmail.com')
+  const passInput =  screen.getByTestId("password-input");
+  userEvent.type(passInput, '1234567') 
+  const loginButton =  screen.getByTestId("login-submit-btn");
+  userEvent.click(loginButton);
+  const drinkButton = await screen.findByTestId(/drinks-bottom-btn/i);
+  userEvent.click(drinkButton);
+  history.push('/foods/53060/in-progress');
+
+  const img = await screen.findByTestId(/recipe-photo/i);
+  expect(img).toBeInTheDocument();
+  const recipeTitle = await screen.findByTestId(/recipe-title/i);
+  expect(recipeTitle).toBeInTheDocument();
+  const recipeCategory = await screen.findByTestId(/recipe-category/i);
+  expect(recipeCategory).toBeInTheDocument();
+  expect(global.fetch).toHaveBeenCalled();
+  
+  const favButton = screen.getByTestId(/favorite-btn/i);
+  expect(favButton).toBeInTheDocument();
+  userEvent.click(favButton);
+  expect(favButton).toBeInTheDocument();
+});
+
+it('teste a função de favoritar na página /drinks', async () => {   
+  const {history} = renderWithRouter(<Provider> <App /> </Provider>);
+  const emailInput =  screen.getByTestId("email-input");
+  userEvent.type(emailInput, 'lalala@gmail.com')
+  const passInput =  screen.getByTestId("password-input");
+  userEvent.type(passInput, '1234567') 
+  const loginButton =  screen.getByTestId("login-submit-btn");
+  userEvent.click(loginButton);
+  const drinkButton = await screen.findByTestId(/drinks-bottom-btn/i);
+  userEvent.click(drinkButton);
+  history.push('drinks/178319/in-progress');
+
+  const img = await screen.findByTestId(/recipe-photo/i);
+  expect(img).toBeInTheDocument();
+  const recipeTitle = await screen.findByTestId(/recipe-title/i);
+  expect(recipeTitle).toBeInTheDocument();
+  const recipeCategory = await screen.findByTestId(/recipe-category/i);
+  expect(recipeCategory).toBeInTheDocument();
+  expect(global.fetch).toHaveBeenCalled();
+  
+  const favButton = screen.getByTestId(/favorite-btn/i);
+  expect(favButton).toBeInTheDocument();
+  userEvent.click(favButton);
+  expect(favButton).toBeInTheDocument();
+});
